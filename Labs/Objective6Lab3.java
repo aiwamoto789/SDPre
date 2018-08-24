0d9d35903c31357950302b16b8697f34cf724ce3
Andy Iwamoto <aiwamoto789@gmail.com>
2018-08-23 21:48:42 -0600

    adding Objective6Lab3.java to Labs folder.

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,17 @@
+
+public class Objective6Lab3{
+  public static void main(String[] args){
+    int counter = 1;
+
+    while(counter <= 20){
+      System.out.print(counter);
+      if(counter % 2 == 0){
+        System.out.println(" is even");
+      }
+      else{
+        System.out.println(" is odd");
+      }
+      counter++;
+    }
+  }
+}
